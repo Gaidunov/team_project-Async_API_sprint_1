c0c016566d0ab3e65559d2d054e78640111e165f
Belyaev <tvaayi@gmail.com>
2022-10-05 18:50:34 +0700

    08: store in redis responses

--- fastapi-solution/src/api/v1/films.py
+++ fastapi-solution/src/api/v1/films.py
@@ -1,6 +1,5 @@
 from http import HTTPStatus
-
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import Query
 
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
@@ -12,10 +11,10 @@ router = APIRouter()
 
 
 class Film(BaseModel):
-    """response model"""
     id: str
     title: str
     imdb_rating: float = 0
+
 
 class Films(BaseModel):
     pagination: Dict
@@ -26,33 +25,45 @@ class Films(BaseModel):
 async def get_movie_by_id(film_id, film_service: FilmService = Depends(get_film_service)):
     film = await film_service.get_by_id(film_id)
     if not film:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='film not found'
+        )
 
     return Film(**film.dict())
 
+
 @router.get('/search/')
 async def search_movie_by_word(
-        search_word: str,
-        page_size: int = Query(ge=1, le=100, default=10),
-        page_number: int = Query(default=0, ge=0),
-        film_service: FilmService = Depends(get_film_service),
+    search_word: str,
+    page_size: int = Query(ge=1, le=100, default=10),
+    page_number: int = Query(default=0, ge=0),
+    film_service: FilmService = Depends(get_film_service),
 ) -> Films:
-    films = await film_service.get_by_search_word(search_word, page_size=page_size, page_number=page_number)
+    films = await film_service.get_by_search_word(
+        search_word,
+        page_size=page_size,
+        page_number=page_number
+    )
 
     return Films(pagination=films['pagination'], result=films['result'])
 
 
 @router.get('/')
 async def get_all_movies(
-        page_size: int = Query(ge=1, le=100, default=10),
-        page_number: int = Query(default=0, ge=0),
-        sort: str = Query(
-            default='imdb_rating',
-            regex='^-?(imdb_rating|title)',
-            description='You can use only: imdb_rating, -imdb_rating'),
-        film_service: FilmService = Depends(get_film_service),
+    page_size: int = Query(ge=1, le=100, default=10),
+    page_number: int = Query(default=0, ge=0),
+    sort: str = Query(
+        default='imdb_rating',
+        regex='^-?(imdb_rating|title)',
+        description='You can use only: imdb_rating, -imdb_rating'),
+    film_service: FilmService = Depends(get_film_service),
 ) -> Films:
 
-    films = await film_service.get_films(page_size=page_size, page_number=page_number, order_by=sort)
+    films = await film_service.get_films(
+        page_size=page_size,
+        page_number=page_number,
+        order_by=sort
+    )
 
     return Films(pagination=films['pagination'], result=films['result'])
