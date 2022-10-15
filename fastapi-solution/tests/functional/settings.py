68330dce9f2ea77b05c32d2ecaf1a313b1811291
root <root@LAPTOP-NB6QNUMI.localdomain>
2022-10-15 19:38:40 +0300

    tests config

--- fastapi-solution/tests/functional/settings.py
+++ fastapi-solution/tests/functional/settings.py
@@ -1,14 +1,16 @@
-from pydantic import BaseSettings, ...
+import os
+
+from pydantic import BaseSettings
+
+from .testdata import ES_MOVIES_MAPPING
 
 
 class TestSettings(BaseSettings):
-    es_host: str = Field('http://127.0.0.1:9200', env='ELASTIC_HOST')
-    es_index: str = 
-    es_id_field: str = 
-    es_index_mapping: dict = 
-
-    redis_host: str = 
-    service_url: str =
+    es_host: str = os.environ['ELASTIC_URL']
+    es_index: str = os.environ['ELASTIC_INDEX_TO_TEST']
+    es_id_field: str = 'id'
+    es_index_mapping: dict = ES_MOVIES_MAPPING
+    service_url: str = os.environ.get('SERVICE_URL', 'http://127.0.0.1:80')
  
 
 test_settings = TestSettings()
